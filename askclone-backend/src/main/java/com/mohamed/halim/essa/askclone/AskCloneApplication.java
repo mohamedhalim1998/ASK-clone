5cc803e6b97310f0bc774b68d2f0a5b96f3e2dc2
MohamedHalim <mohamedhalim1998@gmail.com>
2022-07-11 14:49:12 +0200

    ADD: init backend project with spring

---
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/AskCloneApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.essa.askclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskCloneApplication.class, args);
+	}
+
+}
---
+++ askclone-backend/src/test/java/com/mohamed/halim/essa/askclone/AskCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.essa.askclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
