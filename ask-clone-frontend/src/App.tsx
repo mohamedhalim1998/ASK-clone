24b86f8a2ce7de30715c92e9b6a9419dff11cc76
MohamedHalim <mohamedhalim1998@gmail.com>
2022-07-11 14:44:48 +0200

    ADD: init front end project with typescript and tailwind

---
+++ ask-clone-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="w-10/12 mx-auto">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
