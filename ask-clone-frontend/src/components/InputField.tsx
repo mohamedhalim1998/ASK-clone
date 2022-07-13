70ea73661025524e7c31b59a79d8a391d8a015bf
MohamedHalim <mohamedhalim1998@gmail.com>
2022-07-13 22:14:01 +0200

    ADD: implement signup functionality

--- ask-clone-frontend/src/components/InputField.tsx
+++ ask-clone-frontend/src/components/InputField.tsx
@@ -1,18 +1,22 @@
 interface InputFieldParams {
   label: string;
   type: string;
+  value: string;
   placeholder: string;
+  onChange: (newVal: string) => void;
 }
-const InputField: React.FC<
-  InputFieldParams & React.HTMLAttributes<HTMLDivElement>
-> = (params) => {
+const InputField: React.FC<InputFieldParams> = (params) => {
   return (
     <div>
       <h5 className="text-xs font-semibold mt-4 mb-1">{params.label}</h5>
       <input
         type={params.type}
         placeholder={params.placeholder}
-        className="border py-3 px-2 w-full focus:outline-none"
+        onChange={(e) => {
+          params.onChange(e.currentTarget.value);
+        }}
+        value={params.value}
+        className="border py-3 px-2 w-full rounded-sm focus:outline-none"
       />
     </div>
   );
--- ask-clone-frontend/src/pages/Signup.tsx
+++ ask-clone-frontend/src/pages/Signup.tsx
@@ -1,9 +1,91 @@
+import axios, { AxiosError } from "axios";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
-import Checkbox from "../components/Checkbox";
 import InputField from "../components/InputField";
 import logo from "../logo.png";
 
+interface SignupForm {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
+
 const Signup: React.FC = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  } as SignupForm);
+  const changeUsername = (val: string) => {
+    const data = { ...formData };
+    data.username = val;
+    setFormData(data);
+  };
+  const changeEmail = (val: string) => {
+    const data = { ...formData };
+    data.email = val;
+    setFormData(data);
+  };
+  const changePassword = (val: string) => {
+    const data = { ...formData };
+    data.password = val;
+    setFormData(data);
+  };
+  const changePasswordConfirm = (val: string) => {
+    const data = { ...formData };
+    data.passwordConfirm = val;
+    setFormData(data);
+  };
+
+  const validateForm = () => {
+    toast.dismiss();
+    if (formData.username.length === 0) {
+      toast.error("user name is required");
+      return;
+    }
+    if (formData.email.length === 0) {
+      toast.error("email is required");
+      return;
+    }
+    if (
+      formData.password.length === 0 &&
+      formData.passwordConfirm.length === 0
+    ) {
+      toast.error("passwords is required");
+      return;
+    }
+    if (formData.password !== formData.passwordConfirm) {
+      toast.error("passwords aren't matched");
+      return;
+    }
+    if (!isValidEmail(formData.email)) {
+      toast.error("email is not valid");
+      return;
+    }
+    registerUser();
+  };
+  function isValidEmail(email: string) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+  const registerUser = async () => {
+    console.log("register user");
+
+    axios
+      .post("http://localhost:8080/user/signup", {
+        username: formData.username,
+        email: formData.email,
+        password: formData.password,
+      })
+      .then((response) => {
+        toast.success("signup sucessfully");
+      })
+      .catch((error: AxiosError) => {
+        toast.error("" + error.response?.data);
+      });
+  };
+
   return (
     <div
       style={{
@@ -26,19 +108,49 @@ const Signup: React.FC = () => {
               Log in
             </Link>
           </p>
-          <InputField label="Username" type="text" placeholder="Username" />
-          <InputField label="email" type="email" placeholder="email" />
-          <InputField label="Password" type="password" placeholder="Password" />
+          <InputField
+            label="Username"
+            type="text"
+            placeholder="Username"
+            value={formData.username}
+            onChange={changeUsername}
+          />
+          <InputField
+            label="email"
+            type="email"
+            placeholder="email"
+            value={formData.email}
+            onChange={changeEmail}
+          />
+          <InputField
+            label="Password"
+            type="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={changePassword}
+          />
           <InputField
             label="Confirm Password"
             type="password"
             placeholder="Confirm Password"
+            value={formData.passwordConfirm}
+            onChange={changePasswordConfirm}
           />
-          <button className="w-full text-white bg-accent hover:bg-accentdark rounded-md py-2 my-3 cursor-pointer ">
+          <button
+            className="w-full text-white bg-accent hover:bg-accentdark rounded-md py-2 my-3 cursor-pointer "
+            onClick={(e) => {
+              validateForm();
+            }}
+          >
             Sign Up
           </button>
         </div>
       </div>
+      <Toaster
+        toastOptions={{
+          duration: 2000,
+        }}
+      />
     </div>
   );
 };
