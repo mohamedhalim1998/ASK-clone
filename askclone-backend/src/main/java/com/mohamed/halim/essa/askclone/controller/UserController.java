b696ca7fc132173e4244538c7dc7057350d94736
MohamedHalim <mohamedhalim1998@gmail.com>
2022-07-13 16:28:39 +0200

    ADD: enable CORS

--- askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/controller/UserController.java
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/controller/UserController.java
@@ -13,9 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
