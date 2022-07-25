80a1cef0d409a0f85a8429e898d4ecc654190f33
MohamedHalim <mohamedhalim1998@gmail.com>
2022-07-25 16:29:49 +0200

    ADD: auth test to sign up and login

---
+++ askclone-backend/src/test/java/com/mohamed/halim/essa/askclone/authTests.java
@@ -0,0 +1,146 @@
+package com.mohamed.halim.essa.askclone;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mohamed.halim.essa.askclone.model.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class authTests {
+
+   @Autowired
+   MockMvc mockMvc;
+
+   @Test
+   void test_signup() throws Exception {
+      User user = new User();
+      user.setUsername("testUser");
+      user.setEmail("e@e.com");
+      user.setPassword("test");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated());
+   }
+
+   @Test
+   void test_signupWithWrongParams() throws Exception {
+      User user = new User();
+      user.setUsername("testUser");
+      user.setPassword("test");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().is4xxClientError());
+   }
+
+   @Test
+   void test_signupWithDublicatedEmail() throws Exception {
+      User user = new User();
+      user.setUsername("testUser");
+      user.setEmail("e@e.com");
+      user.setPassword("test");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated());
+
+      user.setUsername("testUser2");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isConflict())
+            .andExpect(jsonPath("$").value("email already used"));
+
+   }
+
+   @Test
+   void test_signupWithDublicatedUsername() throws Exception {
+      User user = new User();
+      user.setUsername("testUser");
+      user.setEmail("e@e.com");
+      user.setPassword("test");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated());
+
+      user.setEmail("e@m.com");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isConflict())
+            .andExpect(jsonPath("$").value("user already used"));
+
+   }
+
+   @Test
+   void test_login() throws Exception {
+      User user = new User();
+      user.setUsername("testUser");
+      user.setEmail("e@e.com");
+      user.setPassword("test");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated());
+
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/login")
+                  .content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+   }
+
+   @Test
+   void test_loginFail() throws Exception {
+      User user = new User();
+      user.setUsername("testUser");
+      user.setEmail("e@e.com");
+      user.setPassword("test");
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/signup").content(asJsonString(user))
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated());
+      user.setUsername("testUser2");
+
+      mockMvc
+            .perform(MockMvcRequestBuilders.post("/user/login")
+                  .content(asJsonString(user))
+
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnauthorized());
+
+   }
+
+   static String asJsonString(final Object obj) {
+      try {
+         return new ObjectMapper().writeValueAsString(obj);
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+}
