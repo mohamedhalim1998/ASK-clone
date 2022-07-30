71603c44e27127d7a0384bf66b7782c883f163e7
MohamedHalim <mohamedhalim1998@gmail.com>
2022-07-30 15:28:34 +0200

    ADD: default values for fields

--- askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/model/Profile.java
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/model/Profile.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -14,36 +15,43 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table
+@Builder(toBuilder = true)
 public class Profile {
-   @Column(name = "profile_username")
    @Id
    private String username;
    @OneToOne
-   @JoinColumn(name = "profile_username", referencedColumnName = "username")
+   @JoinColumn(name = "username", referencedColumnName = "username")
    private AppUser user;
    private String displayname;
    @Enumerated(EnumType.STRING)
-   private Status status;
+   @Builder.Default
+   private Status status = Status.ONLINE;
    @OneToMany(cascade = CascadeType.ALL, mappedBy = "to")
-   private List<Answer> answers;
+   @Builder.Default
+   private List<Answer> answers = List.of();
    @OneToMany(cascade = CascadeType.ALL, mappedBy = "follower")
-   private List<Follower> followers;
+   @Builder.Default
+   private List<Follower> followers = List.of();
    @OneToMany(cascade = CascadeType.ALL, mappedBy = "followee")
-   private List<Follower> followees;
+   @Builder.Default
+   private List<Follower> followees = List.of();
    private String location;
    private String bio;
    private String links;
    private Date birthday;
    @Enumerated(EnumType.STRING)
-   private Gender gender;
-   @Column(columnDefinition = "boolean default true")
+   @Builder.Default
+   private Gender gender = Gender.MALE;
    private boolean allowAnoymousQuestions;
    private String profilePictureUrl;
    private String coverPictureUrl;
