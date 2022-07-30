b00df1fe9c5e39d9b0fcabb1f8dce0af724bd662
MohamedHalim <mohamedhalim1998@gmail.com>
2022-07-30 15:23:13 +0200

    ADD: filter to log requests

---
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/config/LogFilter.java
@@ -0,0 +1,18 @@
+package com.mohamed.halim.essa.askclone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LogFilter {
+   @Bean
+   public CommonsRequestLoggingFilter requestLoggingFilter() {
+      CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+      loggingFilter.setIncludeClientInfo(true);
+      loggingFilter.setIncludeQueryString(true);
+      loggingFilter.setIncludePayload(true);
+      loggingFilter.setMaxPayloadLength(64000);
+      return loggingFilter;
+   }
+}
