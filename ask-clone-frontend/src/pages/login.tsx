86056e33218a3c483258f238db28e752cb7f2bc9
MohamedHalim <mohamedhalim1998@gmail.com>
2022-08-15 22:09:29 +0200

    EDIT: use yup and react form hook instead of manual managment to the login form

--- ask-clone-frontend/src/pages/login.tsx
+++ ask-clone-frontend/src/pages/login.tsx
@@ -7,6 +7,8 @@ import logo from "../logo.png";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { login } from "../store/AuthReducer";
 import { RootState } from "../store/Store";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as Yup from "yup";
 
 interface LoginForm {
   username: string;
@@ -24,25 +26,33 @@ function Login() {
     }
   }, [token]);
 
-  const [formData, setFormData] = useState({
+  const initData: LoginForm = {
     username: "",
     password: "",
     rememberme: false,
-  } as LoginForm);
-  const changeName = (val: string) => {
-    const data = { ...formData };
-    data.username = val;
-    setFormData(data);
   };
-  const changePassword = (val: string) => {
-    const data = { ...formData };
-    data.password = val;
-    setFormData(data);
-  };
-  const changeRememberMe = (val: boolean) => {
-    const data = { ...formData };
-    data.rememberme = val;
-    setFormData(data);
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required(),
+    password: Yup.string().required(),
+    rememberme: Yup.boolean().required(),
+  });
+
+  const { register, handleSubmit, watch } = useForm<LoginForm>({
+    defaultValues: initData,
+  });
+  const onSubmit: SubmitHandler<LoginForm> = (data) => {
+    console.log(data);
+    validationSchema
+    .validate(data)
+    .then((values) => {
+        dispatch(login(data.username, data.password));
+    })
+    .catch((reason) => {
+      console.log(reason);
+      toast.error("Some fields are missing")
+    });
+  
   };
 
   return (
@@ -67,48 +77,39 @@ function Login() {
               Sign up
             </Link>
           </p>
-
-          <InputField
-            label="Login"
-            type="text"
-            placeholder="Username or e-mail"
-            value={formData.username}
-            onChange={changeName}
-          />
-          <InputField
-            label="Password"
-            type="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={changePassword}
-          />
-
-          <div className="flex flex-row w-full justify-between mt-4 mb-1">
-            <Checkbox
-              label="Remember me"
-              isChecked={formData.rememberme}
-              setIsChecked={changeRememberMe}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <InputField
+              label="Login"
+              type="text"
+              placeholder="Username or e-mail"
+              register={register("username")}
             />
-            <Link className="text-xs  text-accent cursor-pointer" to={"/"}>
-              Forget password?
-            </Link>
-          </div>
-          <button
-            className="w-full text-white bg-accent hover:bg-accentdark rounded-md py-2 my-3 cursor-pointer "
-            onClick={() => {
-              console.log("login");
-              dispatch(login(formData.username, formData.password));
-            }}
-          >
-            Log In
-          </button>
+            <InputField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              register={register("password")}
+            />
+
+            <div className="flex flex-row w-full justify-between mt-4 mb-1">
+              <Checkbox
+                label="Remember me"
+                isChecked={watch("rememberme")}
+                register={register("rememberme", { value: false })}
+              />
+              <Link className="text-xs  text-accent cursor-pointer" to={"/"}>
+                Forget password?
+              </Link>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-accent hover:bg-accentdark rounded-md py-2 my-3 cursor-pointer "
+            >
+              Log In
+            </button>
+          </form>
         </div>
       </div>
-      <Toaster
-        toastOptions={{
-          duration: 2000,
-        }}
-      />
     </div>
   );
 }
