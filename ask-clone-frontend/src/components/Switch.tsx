bde4967705ce171630ce2a9dae45ebbba198b848
MohamedHalim <mohamedhalim1998@gmail.com>
2022-08-15 22:15:56 +0200

    ADD: state mangment for switch

--- ask-clone-frontend/src/components/Switch.tsx
+++ ask-clone-frontend/src/components/Switch.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
-function Switch() {
-  return (
-    <label className="relative inline-block w-8 h-4">
-    <input className='hidden' type="checkbox" />
-    <span className="absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-gray-400 duration-500 round slider "></span>
-  </label>
-  )
+import React, { FC } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+interface SwitchParams {
+  checked?: boolean;
+  register?: UseFormRegisterReturn;
 }
 
-export default Switch
+const Switch: FC<SwitchParams> = (params) => {
+  return (
+    <label className="relative inline-block w-8 h-4 my-auto">
+      <input
+        className="hidden"
+        type="checkbox"
+        checked={params.checked}
+        {...params.register}
+      />
+      <span className="absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-gray-400 duration-500 round slider "></span>
+    </label>
+  );
+};
+
+export default Switch;
