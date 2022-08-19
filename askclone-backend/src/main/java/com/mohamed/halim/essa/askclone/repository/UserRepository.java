753cdf5fb614ba7738ca2bfc8b1a23e3d03806f3
MohamedHalim <mohamedhalim1998@gmail.com>
2022-08-19 22:48:37 +0200

    EDIT: remove jdbc data dependency

--- askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/repository/UserRepository.java
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/repository/UserRepository.java
@@ -2,15 +2,16 @@ package com.mohamed.halim.essa.askclone.repository;
 
 import com.mohamed.halim.essa.askclone.model.AppUser;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, String> {
-   @Query("SELECT u FROM user u WHERE u.email = ?1")
-   public AppUser findByEmail(String email);
+   @Query("SELECT u FROM AppUser u WHERE u.email = :email")
+   public AppUser findByEmail(@Param("email") String email);
 
-   @Query("SELECT u FROM user u WHERE u.username = ?1")
-   public AppUser findByUsername(String username);
+   @Query("SELECT u FROM AppUser u WHERE u.username = :username")
+   public AppUser findByUsername(@Param("username") String username);
 }
