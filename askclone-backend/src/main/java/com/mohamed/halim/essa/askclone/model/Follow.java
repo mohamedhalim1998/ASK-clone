702f04df67deebea55dc299287d0008447c4e501
MohamedHalim <mohamedhalim1998@gmail.com>
2022-08-19 19:45:48 +0200

    EDIT: name of follower entity

--- askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/model/Follow.java
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/model/Follow.java
@@ -7,14 +7,18 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table
-public class Follower {
+@Builder
+public class Follow {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
