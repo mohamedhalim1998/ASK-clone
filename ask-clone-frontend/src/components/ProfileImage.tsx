ad46fdf6956b0d1bec95f514aae0e9765ffb327b
MohamedHalim <mohamedhalim1998@gmail.com>
2022-09-16 19:04:52 +0200

    EDIT: load default profile image if none provided

--- ask-clone-frontend/src/components/ProfileImage.tsx
+++ ask-clone-frontend/src/components/ProfileImage.tsx
@@ -3,7 +3,7 @@ const ProfileImage = (url?: string) => {
     <div
       className="rounded-full bg-center w-10 h-10 bg-cover"
       style={{
-        backgroundImage: ` url(http://localhost:8080/image/${url})`,
+        backgroundImage: ` url(http://localhost:8080/image/${url ? url : "blank-profile-pic.png"})`,
       }}
     />
   );
