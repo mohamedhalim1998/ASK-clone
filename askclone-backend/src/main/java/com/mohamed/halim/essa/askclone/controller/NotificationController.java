da339ec8d02f7d25d7b1345107102bc7dd434e04
MohamedHalim <mohamedhalim1998@gmail.com>
2022-10-06 14:58:50 +0200

    ADD: implement notification controller

---
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/controller/NotificationController.java
@@ -0,0 +1,44 @@
+package com.mohamed.halim.essa.askclone.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mohamed.halim.essa.askclone.model.dto.NotificationDto;
+import com.mohamed.halim.essa.askclone.services.NotificationService;
+import com.mohamed.halim.essa.askclone.utils.JwtUtils;
+
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+   private NotificationService service;
+
+   public NotificationController(NotificationService service) {
+      this.service = service;
+   }
+
+   @PostMapping
+   public void markAllAsRead(HttpServletRequest request, @PathVariable(required = false) Long id)
+         throws IllegalAccessException {
+      String username = JwtUtils.extractUsername(request);
+      service.markAllAsRead(username);
+
+   }
+
+   @GetMapping
+   public List<NotificationDto> getAllUserNotifications(HttpServletRequest request,
+         @PathVariable(required = false) Long id)
+         throws IllegalAccessException {
+      String username = JwtUtils.extractUsername(request);
+      return service.getAllNotifications(username);
+   }
+
+}
