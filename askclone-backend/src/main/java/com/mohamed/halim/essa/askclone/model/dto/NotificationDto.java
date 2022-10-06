ce9e465c81c5ccce36653a0fa86898d0090a0e09
MohamedHalim <mohamedhalim1998@gmail.com>
2022-10-06 14:58:13 +0200

    ADD: implement notification dto

---
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/model/dto/NotificationDto.java
@@ -0,0 +1,47 @@
+package com.mohamed.halim.essa.askclone.model.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.mohamed.halim.essa.askclone.model.Notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class NotificationDto {
+   private long id;
+   private String from;
+   private String fromFullName;
+   private String fromProfileImage;
+   private String to;
+   private boolean read;
+   private String type;
+   private long date;
+   private long questionId;
+   private String questionText;
+
+   public static NotificationDto fromNotification(Notification notification) {
+      return NotificationDto.builder()
+            .id(notification.getId())
+            .from(notification.getFrom() != null ? notification.getFrom().getUsername() : null)
+            .fromFullName(notification.getFrom() != null ? notification.getFrom().getDisplayname() : null)
+            .fromProfileImage(notification.getFrom() != null ? notification.getFrom().getProfilePictureUrl() : null)
+            .to(notification.getTo().getUsername())
+            .type(notification.getNotificationType().toString())
+            .read(notification.isRead())
+            .date(notification.getDate().getTime())
+            .questionId(notification.getQuestion().getId())
+            .questionText(notification.getQuestion().getQuestion())
+            .build();
+   }
+
+   public static List<NotificationDto> fromNotificationList(List<Notification> likes) {
+      return likes.stream().map(NotificationDto::fromNotification).collect(Collectors.toList());
+   }
+}
