1fbbcc3d9c9f63be5a7ae36e41cec0d35146c362
MohamedHalim <mohamedhalim1998@gmail.com>
2022-10-06 14:57:38 +0200

    ADD: implement notification  repository

---
+++ askclone-backend/src/main/java/com/mohamed/halim/essa/askclone/repository/NotificationRepository.java
@@ -0,0 +1,20 @@
+package com.mohamed.halim.essa.askclone.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.mohamed.halim.essa.askclone.model.Notification;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+   @Query("SELECT n FROM Notification n WHERE n.to.username = :username AND n.read = FALSE")
+   List<Notification> findUnreadNotificationByUsername(@Param("username") String username);
+
+   @Query("SELECT n FROM Notification n WHERE n.to.username = :username")
+   List<Notification> findAllNotificationByUsername(@Param("username") String username);
+}
