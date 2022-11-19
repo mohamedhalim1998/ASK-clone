3b196cb4c325aa046c3756d9741acc6dfea62b3c
MohamedHalim <mohamedhalim1998@gmail.com>
2022-11-20 00:26:38 +0200

    ADD: implement all users search

--- ask-clone-frontend/src/pages/FriendsPage.tsx
+++ ask-clone-frontend/src/pages/FriendsPage.tsx
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import ProfileImage from "../components/ProfileImage";
 import { Friend } from "../model/Friend";
-import { getFriends } from "../store/FirendsReducer";
+import { getFriends, searchFriends } from "../store/FirendsReducer";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { RightArrow, SearchIcon } from "../utils/Icons";
+import Switch from "../components/Switch";
+import { followUser } from "../store/ProfileReducer";
 
 function FriendsPage() {
   const dispatch = useAppDispatch();
   const allFriends: Friend[] = useAppSelector((state) => state.friends.friends);
+  const otherUsers: Friend[] = useAppSelector(
+    (state) => state.friends.otherUsers
+  );
   const [friends, setFriends] = useState<Friend[]>(allFriends);
+  const [searchAll, setSearchAll] = useState<boolean>(false);
   const loading = useAppSelector((state) => state.friends.loading);
   useEffect(() => {
     console.log("get friends");
     dispatch(getFriends());
-  }, [loading]);
+  }, [loading, friends]);
   if (loading) {
     return <div>loading</div>;
   }
@@ -27,7 +33,6 @@ function FriendsPage() {
           <SearchInput
             onChange={(query: string) => {
               console.log("search friends");
-
               setFriends(
                 query.length !== 0
                   ? [
@@ -37,13 +42,30 @@ function FriendsPage() {
                     ]
                   : [...allFriends]
               );
+              if (searchAll) {
+                dispatch(searchFriends(query));
+              }
             }}
           />
-          <h4 className="mt-10 mb-2 font-semibold text-sm">Your Friends</h4>
+          <div className="flex flex-row py-2">
+            <Switch
+              checked={searchAll}
+              onChange={() => {
+                setSearchAll(!searchAll);
+              }}
+            />
+            <p className="pl-4 text-xs">Also search all users</p>
+          </div>
+          <h4 className="mt-5 mb-2 font-semibold text-sm">Your Friends</h4>
 
           {friends.map((f, index) => (
             <FriendCard {...f} key={index} />
           ))}
+          {searchAll && (
+            <h4 className="mt-5 mb-2 font-semibold text-sm">other users</h4>
+          )}
+          {searchAll &&
+            otherUsers.map((f, index) => <FriendCard {...f} key={index} />)}
         </div>
       </div>
     </div>
@@ -63,13 +85,14 @@ const SearchInput = (params: { onChange: (query: string) => void }) => {
         }}
         type="text"
         placeholder="Search People "
-        className="border-none focus:outline-none text-base"
+        className="border-none focus:outline-none text-base w-full"
       />
     </div>
   );
 };
 
 const FriendCard = (friend: Friend) => {
+  const dispatch = useAppDispatch();
   return (
     <div className="flex flex-row w-full py-2">
       {ProfileImage(friend.profilePic)}
@@ -77,13 +100,25 @@ const FriendCard = (friend: Friend) => {
         <p className="font-semibold">{friend.fullname}</p>
         <p className="text-gray-500">{friend.username}</p>
       </div>
-      <Link
-        className="flex flex-row border-2 border-themeblack rounded-md hover:text-white hover:bg-themeblack h-fit my-auto ml-auto pl-2 pr-1 py-1 cursor-pointer"
-        to={`/user/${friend.username}/ask`}
-      >
-        <p className="py-auto text-sm font-semibold">Ask</p>
-        <RightArrow />
-      </Link>
+      <div className="flex flex-row  h-fit my-auto ml-auto">
+        <Link
+          className="flex flex-row border-2 border-themeblack rounded-md hover:text-white hover:bg-themeblack mx-2 pl-2 pr-1 py-1 cursor-pointer"
+          to={`/user/${friend.username}/ask`}
+        >
+          <p className="py-auto text-sm font-semibold">Follow</p>
+          <RightArrow />
+        </Link>
+
+        <div
+          className="flex flex-row border-2 border-themeblack rounded-md hover:text-white hover:bg-themeblack mx-2 pl-2 pr-1 py-1 cursor-pointer"
+          onClick={() => {
+            dispatch(followUser(friend.username));
+          }}
+        >
+          <p className="py-auto text-sm font-semibold">Ask</p>
+          <RightArrow />
+        </div>
+      </div>
     </div>
   );
 };
--- ask-clone-frontend/src/store/FirendsReducer.ts
+++ ask-clone-frontend/src/store/FirendsReducer.ts
@@ -11,6 +11,9 @@ import { RootState } from "./Store";
 
 export const updateFriendsList =
   createAction<AxiosResponse>("updateFriendsList");
+export const updateOtherUsersList = createAction<AxiosResponse>(
+  "updateOtherUsersList"
+);
 export const updateFriendsLoading = createAction<boolean>(
   "updateFriendsLoading"
 );
@@ -19,6 +22,13 @@ export const getFriends = () => {
     url: "http://localhost:8080/friends",
     useJwtToken: true,
     onSuccess: updateFriendsList.toString(),
+  });
+};
+export const searchFriends = (query: string) => {
+  return apiCall({
+    url: `http://localhost:8080/profile/search?query=${query}`,
+    useJwtToken: true,
+    onSuccess: updateOtherUsersList.toString(),
   });
 };
 
@@ -31,16 +41,26 @@ export const friendSelector = (username: string) =>
 
 interface FriendsState {
   friends: Friend[];
+  otherUsers: Friend[];
   loading: boolean;
 }
 const initState: FriendsState = {
   friends: [],
+  otherUsers: [],
   loading: true,
 };
 export default createReducer<FriendsState>(initState, {
   [updateFriendsList.type]: (state, action: PayloadAction<AxiosResponse>) => {
     console.log("get friends");
     state.friends = action.payload.data;
+    state.loading = false;
+  },
+  [updateOtherUsersList.type]: (
+    state,
+    action: PayloadAction<AxiosResponse>
+  ) => {
+    console.log("get friends");
+    state.otherUsers = action.payload.data;
     state.loading = false;
   },
   [updateFriendsLoading.type]: (state, action: PayloadAction<boolean>) => {
